eeeaa1c7d9224914ca3cd1b2b732112d47b409d7
Rafael Silva <Rafael Silva>
2017-03-12 23:29:13 -0300

    Creates processors for some signals

--- src/com/compiler/Scanner.java
+++ src/com/compiler/Scanner.java
@@ -9,72 +9,137 @@ public class Scanner {
         char character = line.charAt(cursor.getColumn());
         StringBuffer token = new StringBuffer(String.valueOf(character));
 
-        if (Character.isDigit(character)){
+        if (Character.isDigit(character)) {
 
             token = processDigit(token, line, cursor);
 
-            if(!cursor.eof()){
+            if (!cursor.eof()) {
                 character = line.charAt(cursor.getColumn());
-            }else{
+            } else {
                 return new Token(TokenType.INT, token.toString());
             }
-            if(character == '.'){
+            if (character == '.') {
                 token.append(character);
                 token = processPoint(token, line, cursor);
-                if(token != null){
+                if (token != null) {
                     return new Token(TokenType.FLOAT, token.toString());
-                }else{
+                } else {
                     //TODO malformation
                 }
-            }else{
+            } else {
                 return new Token(TokenType.INT, token.toString());
             }
-        } else if(character == '.'){
+        } else if (character == '.') {
             token = processPoint(token, line, cursor);
-            if(token != null){
+            if (token != null) {
                 return new Token(TokenType.FLOAT, token.toString());
-            }else{
+            } else {
                 //TODO malformation
+            }
+        } else if (character == '+') {
+            cursor.move();
+            return new Token(TokenType.SUM, token.toString());
+        } else if (character == '-') {
+            cursor.move();
+            return new Token(TokenType.SUB, token.toString());
+        } else if (character == '*') {
+            cursor.move();
+            return new Token(TokenType.MULT, token.toString());
+        } else if (character == '<') {
+            cursor.move();
+            if (!cursor.eof()) {
+                character = line.charAt(cursor.getColumn());
+                if (character == '=') {
+                    cursor.move();
+                    token.append(character);
+                    return new Token(TokenType.LESS_OR_EQUAL, token.toString());
+                } else {
+                    return new Token(TokenType.LESS_THAN, token.toString());
+                }
+            } else {
+                return new Token(TokenType.LESS_THAN, token.toString());
+            }
+        } else if (character == '>') {
+            cursor.move();
+            if (!cursor.eof()) {
+                character = line.charAt(cursor.getColumn());
+                if (character == '=') {
+                    cursor.move();
+                    token.append(character);
+                    return new Token(TokenType.GREATER_OR_EQUAL, token.toString());
+                } else {
+                    return new Token(TokenType.GREATER_THAN, token.toString());
+                }
+            } else {
+                return new Token(TokenType.GREATER_THAN, token.toString());
+            }
+        } else if (character == '=') {
+            cursor.move();
+            if (!cursor.eof()) {
+                character = line.charAt(cursor.getColumn());
+                if (character == '=') {
+                    cursor.move();
+                    token.append(character);
+                    return new Token(TokenType.ASSIGNMENT, token.toString());
+                } else {
+                    return new Token(TokenType.EQUALITY, token.toString());
+                }
+            } else {
+                return new Token(TokenType.EQUALITY, token.toString());
+            }
+        } else if (character == '!') {
+            cursor.move();
+            if (!cursor.eof()) {
+                character = line.charAt(cursor.getColumn());
+                if (character == '=') {
+                    cursor.move();
+                    token.append(character);
+                    return new Token(TokenType.DENIAL, token.toString());
+                } else {
+                    return new Token(TokenType.DIFFERENT, token.toString());
+                }
+            } else {
+                return new Token(TokenType.DENIAL, token.toString());
             }
         }
 
         return null;
     }
 
-    private StringBuffer processDigit(StringBuffer token, String line, Cursor cursor){
+    private StringBuffer processDigit(StringBuffer token, String line, Cursor cursor) {
         char character = line.charAt(cursor.getColumn());
 
-        do{
+        do {
             cursor.move();
 
-            if(!cursor.eof()){
+            if (!cursor.eof()) {
                 character = line.charAt(cursor.getColumn());
 
-                if(Character.isDigit(character)){
+                if (Character.isDigit(character)) {
                     token.append(String.valueOf(character));
                 }
             }
-        }while(Character.isDigit(character) && !cursor.eof());
+        } while (Character.isDigit(character) && !cursor.eof());
 
         return token;
     }
 
-    private StringBuffer processPoint(StringBuffer token, String line, Cursor cursor){
+    private StringBuffer processPoint(StringBuffer token, String line, Cursor cursor) {
         char character = line.charAt(cursor.getColumn());
 
         cursor.move();
 
-        if(!cursor.eof()){
+        if (!cursor.eof()) {
             character = line.charAt(cursor.getColumn());
 
-            if(Character.isDigit(character)) {
+            if (Character.isDigit(character)) {
                 token.append(character);
                 token = processDigit(token, line, cursor);
-            }else{
+            } else {
                 //TODO malformation
                 return null;
             }
-        }else{
+        } else {
             //TODO malformation
             return null;
         }
--- src/com/compiler/TokenType.java
+++ src/com/compiler/TokenType.java
@@ -5,5 +5,18 @@ package com.compiler;
  */
 public enum TokenType {
     INT,
-    FLOAT
+    FLOAT,
+    SUM,
+    SUB,
+    MULT,
+    DIV,
+    LESS_THAN,
+    LESS_OR_EQUAL,
+    GREATER_THAN,
+    GREATER_OR_EQUAL,
+    EQUALITY,
+    ASSIGNMENT,
+    DIFFERENT,
+    DENIAL
+
 }
